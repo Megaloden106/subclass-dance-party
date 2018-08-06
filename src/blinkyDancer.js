bfe6fc9e4e587ffeeb9a0446c2b2b5439bfc2123
megaloden106 <eddielo106@gmail.com>
2018-08-06 15:34:25 -0700

    Completed switch from functional to pseudoclassical instantiation.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,22 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  // this = Object.create(BlinkyDancer.prototype)
+  Dancer.apply(this, arguments);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+  // return this
+};
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
 
-  return blinkyDancer;
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.oldStep = function() {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
+
+BlinkyDancer.prototype.step = function() {
+  this.oldStep();
+  this.$node.toggle();
 };
